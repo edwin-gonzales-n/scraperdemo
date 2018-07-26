f94d463aad6807dea807c825f8cbccebe5bbd3c2
Edwin Gonzales <eogonzal@EOGONZALs-MacBook-Pro.local>
2018-07-26 17:28:49 -0500

    adding partially and very dirty working code.

--- src/main/java/com/scraper/demo/DemoApplication.java
+++ src/main/java/com/scraper/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.scraper.demo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlContent;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.springframework.boot.SpringApplication;
@@ -28,7 +29,15 @@ public class DemoApplication {
 //            System.out.println(page.asXml());
 //            System.out.println(page.asText());
             HtmlPage page = client.getPage(baseUrl);
+//            System.out.println(page.asText());
+            System.out.println("This is the start of the page.getByXpath");
+//            System.out.println(page.getByXPath("//tr[@class='athing']"));
+            System.out.println(page.getByXPath("//div"));
+            System.out.println("Title out:");
+            System.out.println(page.getByXPath("//div[@class='fp-info has-description']//div//div"));
+
 //            List<HtmlElement> itemList = page.getByXPath("//tr[@class='athing']");
+//            List<HtmlElement> itemList = page.getByXPath("//div[@class='fp-info has-description']//div//div");
             List<HtmlElement> itemList = page.getByXPath("//div[@id='isotope']");
             if(itemList.isEmpty()){
                 System.out.println("No item found");
@@ -39,24 +48,26 @@ public class DemoApplication {
 //                    String title = ((HtmlElement) htmlItem.getFirstByXPath("./td[not(@valign='top')] [@class='title']")).asText();
 //                    String url = ((HtmlAnchor) htmlItem.getFirstByXPath("./td[not(@valign='top')] [@class='title']/a")).getHrefAttribute();
 //                    String author = ((HtmlElement) htmlItem.getFirstByXPath("./following-sibling:: tr/td[@class='subtext']/a[@class='hnuser']")).asText();
-                    String title = ((HtmlElement) htmlItem.getFirstByXPath("./div[not(@class='fp-title')] [@class='title']")).asText();
-                    String url = ((HtmlAnchor) htmlItem.getFirstByXPath("./following-sibling :: a")).getHrefAttribute();
+                    String title = ((HtmlElement) htmlItem.getFirstByXPath("//h4")).asText();
+//                    System.out.println("Title out:");
+//                    System.out.println(title);
+                    String info = ((HtmlElement) htmlItem.getFirstByXPath("//a//p")).asText();
+//                    System.out.println(info);
+                    String price = ((HtmlElement) htmlItem.getFirstByXPath("//*[@class='fp-info has-description']")).asText();
+                    System.out.println(price);
 //                    String author = ((HtmlElement) htmlItem.getFirstByXPath("./following-sibling:: tr/td[@class='subtext']/a[@class='hnuser']")).asText();
 
 //                    int score = Integer.parseInt(((HtmlElement) htmlItem.getFirstByXPath("./following-sibling:: tr/td[@class='subtext']/span[@class='score'] ")).asText().replace(" points", ""));
 
 //                    HackerNewsItem hnItem = new HackerNewsItem(title, url, author, score, position, id);
-                    HackerNewsItem hnItem = new HackerNewsItem(title, url);
-
-                    ObjectMapper mapper = new ObjectMapper();
-                    String jsonString = mapper.writeValueAsString(hnItem) ;
-//                    7/9
-                    System.out.println(jsonString);
+//                    HackerNewsItem hnItem = new HackerNewsItem(title, info, price);
+//
+//                    ObjectMapper mapper = new ObjectMapper();
+//                    String jsonString = mapper.writeValueAsString(hnItem) ;
+////                    7/9
+//                    System.out.println(jsonString);
                 }
             }
-
-
-
         }
         catch(Exception e){
                 e.printStackTrace();
--- src/main/java/com/scraper/demo/HackerNewsItem.java
+++ src/main/java/com/scraper/demo/HackerNewsItem.java
@@ -3,20 +3,40 @@ package com.scraper.demo;
 public class HackerNewsItem {
     private String title;
     private String url ;
+    private String info;
+    public String price;
     private String author;
     private int score;
     private int position ;
     private int id ;
 
-    public HackerNewsItem(String title, String url)
+    public HackerNewsItem(String title, String info, String price)
     {
         super();
         this.title = title;
-        this.url = url;
+        this.info = info;
+        this.info = price;
+//        this.url = url;
 //        this.author = author;
 //        this.score = score;
 //        this.position = position;
 //        this.id = id;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
     }
 
     public String getTitle() {
@@ -27,43 +47,43 @@ public class HackerNewsItem {
         this.title = title;
     }
 
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
-
-    public int getPosition() {
-        return position;
-    }
-
-    public void setPosition(int position) {
-        this.position = position;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
+//    public String getUrl() {
+//        return url;
+//    }
+//
+//    public void setUrl(String url) {
+//        this.url = url;
+//    }
+//
+//    public String getAuthor() {
+//        return author;
+//    }
+//
+//    public void setAuthor(String author) {
+//        this.author = author;
+//    }
+//
+//    public int getScore() {
+//        return score;
+//    }
+//
+//    public void setScore(int score) {
+//        this.score = score;
+//    }
+//
+//    public int getPosition() {
+//        return position;
+//    }
+//
+//    public void setPosition(int position) {
+//        this.position = position;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
 }
